97eeb32c24df0e6629284a0063f11e1b10a30656
Oleg Kuzmenko <oleg.v.kuzmenko@gmail.com>
2020-08-30 13:59:31 +0300

    Homework 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,26 @@
-//String.prototype.isPalindrome - для задачи №1
+
+String.prototype.isPalindrome = function() {
+    const strStraight = this.split(' ').join('').toLowerCase();
+    const halfLength = Math.floor(strStraight.length / 2);
+    return strStraight.substring(0, halfLength) === strStraight.substring(strStraight.length % 2 === 0 ? halfLength : halfLength + 1).split('').reverse().join('');
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+    if (marks.length === 0) {
+        return 0;
+    }
+    const sum = marks.reduce((acc, mark) => acc += mark, 0);
+    const roundAverage = Math.round(sum / marks.length);
+    return roundAverage;
+};
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+
+function checkBirthday(date) {
+    const currentTime = Date.now();
+    const birthday = +(new Date(date));
+    const diff = currentTime - birthday;
+    const age = diff/ ((3 * 31536000000 + 31622400000) / 4);
+    const verdict = age >= 18;
+    return verdict;
+};
